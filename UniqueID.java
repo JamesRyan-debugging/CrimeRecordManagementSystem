e45c203440088da7e91953d107fc43b6eb36d177
James Ryan B. Magsalay <116859140+Jamesryanmagsalay@users.noreply.github.com>
2022-11-04 20:54:21 +0700

    Create UniqueID.java

---
+++ UniqueID.java
@@ -0,0 +1,21 @@
+
+package crime;
+
+public class UniqueID {
+    
+private int uid;
+   
+    UniqueID(int id)
+    {
+        this.uid = id;
+    }
+   
+    public int getuid()
+    {
+        return uid;
+    }
+
+   
+
+   
+}
